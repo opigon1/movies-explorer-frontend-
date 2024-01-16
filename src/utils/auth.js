0a6356aa1d01bcb02a79d26a346832d34be1e71b
opigon1 <step.rindin@yandex.ru>
2024-01-17 01:38:40 +0300

    url-fix

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://api.movie.diplom.nomoredomainsmonster.ru';
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://api.movie.diplom.nomoredomainsmonster.ru';
+// const BASE_URL = 'http://localhost:3000';
 
 function handleCheckResponse(res) {
   if (res.ok) {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const apiConfig = {
-  // url: "https://api.project.mesto.nomoredomainsmonster.ru/",
-  url: 'http://localhost:3000',
+  url: "https://api.project.mesto.nomoredomainsmonster.ru/",
+  // url: 'http://localhost:3000',
   headers: {
     'content-type': 'application/json',
   },
